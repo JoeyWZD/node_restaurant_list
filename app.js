48f67577ecf5880695148ef48690f77d742bf6e1
JoeyWZD <doomsday0711@gmail.com>
2022-05-15 02:28:25 +0800

    Optimize the variable and the search function

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const port = 3000
 // 設定樣板引擎
 const exphbs = require('express-handlebars')
 
-// 載入餐廳列表的json檔
-const RESTAURANT_LIST = require('./restaurant.json')
+// 載入餐廳列表的json檔，使用解構賦值
+const { results } = require('./restaurant.json')
 
 // 啟用樣板引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -18,23 +18,29 @@ app.use(express.static('public'))
 
 // 渲染首頁
 app.get('/', (req, res) => {
-  res.render('index', { restaurant: RESTAURANT_LIST.results })
+  res.render('index', { restaurant: results })
 })
 
 // 渲染分頁
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = RESTAURANT_LIST.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  const restaurant = results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant })
 })
 
 // 渲染搜尋頁面
 app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  const restaurant = RESTAURANT_LIST.results.filter(restaurant => { return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()) })
-  // if (restaurant.length === 0) {
-  //   alert('查無此餐廳')
-  // }
-  res.render('index', { restaurant: restaurant, keyword: keyword })
+  const keyword = req.query.keyword.trim()
+
+  if (!keyword) {
+    return res.redirect("/")
+  }
+
+  const restaurant = results.filter(restaurant =>
+    restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+    restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  )
+
+  res.render('index', { restaurant, keyword })
 })
 
 
